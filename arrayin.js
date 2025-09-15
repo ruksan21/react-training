5ae51598c061f62000b6b08c3496692976df623a
Ruksan karki <rukshankarki80@gmail.com>
2025-09-15 19:36:12 +0545

    array

--- arrayin.js
+++ arrayin.js
@@ -1,9 +1,10 @@
 const array = ["apple", "banana", "cherry", "date", "fig"];
-let arraylength = array.length;
-console.log(array[arraylength - 1 ]);
-console.log(array.at(-1));
-console.log(array);
-console.log(array.slice(-1));
-console.log(array.unshift("date"));
-console.log(array.push("grape"));
-console.log(array);
+// let arraylength = array.length;
+// console.log(array[arraylength - 1 ]);
+// console.log(array.at(-1));
+// console.log(array);
+console.log(array.slice(3,4));
+console.log(array.splice(3,2));
+// console.log(array.unshift("date"));
+// console.log(array.push("grape"));
+// console.log(array);
